99f80f82a485ac51bca97e6b6a4493e55d4f492c
bianhan258369 <1399634971@qq.com>
2020-10-21 22:16:34 +0800

    .

--- src/main/java/com/example/bianhan/iftttgenerator/service/FormalismService.java
+++ src/main/java/com/example/bianhan/iftttgenerator/service/FormalismService.java
@@ -1,10 +1,9 @@
 package com.example.bianhan.iftttgenerator.service;
 
-import com.example.bianhan.iftttgenerator.pojo.AlwaysNeverRequirement;
-import com.example.bianhan.iftttgenerator.pojo.EnvironmentOntology;
-import com.example.bianhan.iftttgenerator.pojo.OccurenceRequirement;
-import com.example.bianhan.iftttgenerator.pojo.Requirement;
+import com.example.bianhan.iftttgenerator.configuration.PathConfiguration;
+import com.example.bianhan.iftttgenerator.pojo.*;
 import org.dom4j.DocumentException;
+import org.springframework.stereotype.Service;
 import rwth.i2.ltl2ba4j.formula.IFormula;
 import rwth.i2.ltl2ba4j.formula.IFormulaFactory;
 import rwth.i2.ltl2ba4j.formula.impl.FormulaFactory;
@@ -13,12 +12,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import static com.example.bianhan.iftttgenerator.util.ComputeUtil.computeIfThenRequirements;
+import static com.example.bianhan.iftttgenerator.util.ComputeUtil.computeMap;
 import static com.example.bianhan.iftttgenerator.util.ComputeUtil.initRequirements;
 
+@Service("formalismService")
 public class FormalismService {
-    private List<IFormula> toLtl(String requirementTexts, String ontologyPath, int index) throws IOException, DocumentException {
+    public IFormula toLtl(String requirementTexts, String ontologyPath, int index) throws IOException, DocumentException {
         EnvironmentOntology eo = new EnvironmentOntology(ontologyPath);
+        Map<String, String> intendMap = computeMap(PathConfiguration.DROOLSMAPPATH, "intendMap", eo);
         List<Requirement> requirements = initRequirements(Arrays.asList(requirementTexts.split("//")));
         IFormulaFactory factory = new FormulaFactory();
         List<IFormula> formulas = new ArrayList<>();
@@ -59,7 +63,46 @@ public class FormalismService {
             }
         }
 
-        return formulas;
+        List<IfThenRequirement> ifThenRequirements = computeIfThenRequirements(requirements, intendMap, ontologyPath).get(index);
+        for(IfThenRequirement ifThenRequirement : ifThenRequirements){
+            List<String> triggers = ifThenRequirement.getTriggerList();
+            List<String> actions = ifThenRequirement.getActionList();
+            IFormula triggerFormula;
+            IFormula actionFormula;
+            if(triggers.size() == 1) triggerFormula = factory.Proposition(triggers.get(0));
+            else if(triggers.size() == 2){
+                triggerFormula = factory.And(factory.Proposition(triggers.get(0)), factory.Proposition(triggers.get(1)));
+            }
+            else {
+                triggerFormula = factory.And(factory.Proposition(triggers.get(0)), factory.Proposition(triggers.get(1)));
+                for(int i = 2;i < triggers.size();i++) triggerFormula = factory.And(triggerFormula, factory.Proposition(triggers.get(i)));
+            }
+            if(actions.size() == 1) actionFormula = factory.Proposition(actions.get(0));
+            else if(actions.size() == 2){
+                actionFormula = factory.And(factory.Proposition(actions.get(0)), factory.Proposition(actions.get(1)));
+            }
+            else {
+                actionFormula = factory.And(factory.Proposition(actions.get(0)), factory.Proposition(actions.get(1)));
+                for(int i = 2;i < actions.size();i++) actionFormula = factory.And(actionFormula, factory.Proposition(actions.get(i)));
+            }
+            formula = factory.Impl(triggerFormula, factory.X(actionFormula));
+            formulas.add(formula);
+        }
+        if(formulas.size() == 1) return formulas.get(0);
+        else if(formulas.size() == 2) return factory.And(formulas.get(0), formulas.get(1));
+        else {
+            IFormula tempFormula = factory.And(formulas.get(0), formulas.get(1));
+            for(int i = 2;i < formulas.size();i++) tempFormula = factory.And(tempFormula, formulas.get(i));
+            return tempFormula;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        FormalismService formalismService = new FormalismService();
+        String ontologyPath = "ontology_SmartConferenceRoom.xml";
+        EnvironmentOntology eo = new EnvironmentOntology(ontologyPath);
+        String re = "IF Person.distanceFromPro<2 THEN Blind.bclosed,Projector.pon//IF Air.humidity>30 THEN allow ventilating the room";
+        System.out.println(formalismService.toLtl(re, ontologyPath ,0));
     }
 }
 
